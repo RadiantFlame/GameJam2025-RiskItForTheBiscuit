d3d36d9463dfa7ec2cfcfb5e2a79e2509c340084
RadiantFlame <braydenvilliers@gmail.com>
2025-08-27 00:18:36 +0100

    Randomises 3 images upon button press

--- Risk4Bic/Assets/Scripts/SlotMachine.cs
+++ Risk4Bic/Assets/Scripts/SlotMachine.cs
@@ -1,13 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class SlotMachine : MonoBehaviour
 {
-    public Image[] Slots;
+    public UnityEngine.UI.Image[] Slots;
+    public Sprite[] Ingredients;
+    public UnityEngine.UI.Button SpinLever;
 
+    private void Start()
+    {
+       // InvokeRepeating("DoThing", 1.0f, 0.1f);
+    }
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+
+    public void SpinWheels()
+    {
+        for (int i = 0; i < Slots.Length; i++)
+        {
+            Slots[i].sprite = Ingredients[Random.Range(0, 4)];
+        }
+    }
+
+    void DoThing()
+    {
+        for (int i = 0; i < Slots.Length; i++) {
+            Slots[i].sprite = Ingredients[Random.Range(0,4)];
+        }
     }
 }
