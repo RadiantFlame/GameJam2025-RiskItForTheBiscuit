8f4398112df37b5416d1c01402233f653da6e141
RadiantFlame <braydenvilliers@gmail.com>
2025-08-26 23:22:30 +0100

    Made Basic Slotmachine

---
+++ Risk4Bic/Assets/Scripts/SlotMachine.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlotMachine : MonoBehaviour
+{
+    public Image[] Slots;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
